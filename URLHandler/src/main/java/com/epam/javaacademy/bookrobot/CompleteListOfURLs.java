a0b3a4179a2ca58854afa68e41ba6caf8df3fa88
Michal Rozycki <michalrozyck@gmail.com>
2016-03-24 15:30:48 +0100

    Now new URLHandler works with rest of modules

--- URLHandler/src/main/java/com/epam/javaacademy/bookrobot/CompleteListOfURLs.java
+++ URLHandler/src/main/java/com/epam/javaacademy/bookrobot/CompleteListOfURLs.java
@@ -3,111 +3,142 @@ package com.epam.javaacademy.bookrobot;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+
 public class CompleteListOfURLs {
-	
-	Map <URL, ArrayList <String> > mapOfURLs = new HashMap<>();
-	
+
+	static HashSet<String> listOfURLs = new HashSet<>();
+
+	final int lvl = 3;
 	private static final Pattern URLPATTERN = Pattern.compile(
-			"(?:^|[\\W])((ht|f)tp(s?):\\/\\/|www\\.)"
-					+ "(([\\w\\-]+\\.){1,}?([\\w\\-.~]+\\/?)*"
+			"(?:^|[\\W])((ht|f)tp(s?):\\/\\/|www\\.)" + "(([\\w\\-]+\\.){1,}?([\\w\\-.~]+\\/?)*"
 					+ "[\\p{Alnum}.,%_=?&#\\-+()\\[\\]\\*$~@!:/{}']*)",
-					Pattern.CASE_INSENSITIVE | Pattern.MULTILINE | Pattern.DOTALL);
-	
-	public static void main(String[] args) {
-		CompleteListOfURLs test = new CompleteListOfURLs();
-		System.out.println(test.createMap().toString());
-	}
-	
-	
+			Pattern.CASE_INSENSITIVE | Pattern.MULTILINE | Pattern.DOTALL);
 
-	public Map <URL, ArrayList <String> > createMap () {
-		
-		ArrayList<URL> URLs;
-		ListOfURLToDig listOfStartWebsites = new ListOfURLToDig();
-		URLs = listOfStartWebsites.getURLList();
-		
-		for (URL url : URLs) {
-			mapOfURLs.put(url, createURLs(url));
+
+	Map<String, HashSet<String>> createMap() {
+		Map<String, HashSet<String>> urlMap = new HashMap<>();
+		// iteracja po liscie linkow do przeszukania (baza ksiegarni)
+		ListOfURLToDig bookstores = new ListOfURLToDig();
+
+		HashSet<String> firstIterlistOfURLs = new HashSet<>();
+		HashSet<String> sectIterlistOfURLs = new HashSet<>();
+		HashSet<String> thirdIterlistOfURLs = new HashSet<>();
+		HashSet<String> initlistOfURLs = new HashSet<>();
+
+		for (int i = 0; i < bookstores.URLs.size(); i++) {
+
+			String key = bookstores.URLs.get(i).toString();
+			initlistOfURLs.add(key);
+			createURLs(initlistOfURLs);
+			firstIterlistOfURLs.addAll(listOfURLs);
+			createURLs(firstIterlistOfURLs);
+			sectIterlistOfURLs.addAll(listOfURLs);
+		/*	createURLs(sectIterlistOfURLs);
+			thirdIterlistOfURLs.addAll(listOfURLs);
+			createURLs(thirdIterlistOfURLs);*/
+			urlMap.put(key, listOfURLs);
 		}
-		return mapOfURLs;
+
+		return urlMap;
 	}
-	
-	private static ArrayList <String> createURLs (URL url){
-		ArrayList <String> result = null;
-		
+
+	private static void createURLs(HashSet<String> urlString) {
+
 		CheckRobotTxt disallowed = new CheckRobotTxt();
-		HashSet<String> disallowedURLs = disallowed.listOfUnallowedURLs(url.toString());
-		String webContent = downloadPage (url);
-		
-		ArrayList<String> fetchedURLs =  fetchAllURL(webContent);
-		result = filterURLs(fetchedURLs, disallowedURLs);
-				
-		return result;
+		Iterator<String> iter = urlString.iterator();
+
+		try {
+			while (iter.hasNext()) {
+				URL url;
+				String item = iter.next();
+				url = new URL(item);
+				HashSet<String> disallowedURLs = disallowed.listOfUnallowedURLs(item);
+				String webContent = downloadPage(item);
+				String hostName = url.getHost().toLowerCase();
+
+				HashSet<String> fetchedURLs = fetchAllURL(webContent, hostName);
+				listOfURLs.addAll(filterURLs(fetchedURLs, disallowedURLs));
+			}
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		}
+
 	}
 
-	private static ArrayList<String> fetchAllURL(String webContent) {
-		ArrayList<String> result = new ArrayList<String>();
-		
-		Matcher matcher = URLPATTERN.matcher(webContent);
-		while (matcher.find()) {
-		    int matchStart = matcher.start(1);
-		    int matchEnd = matcher.end();
+	private static String downloadPage(String urlString) {
 
-		    String substring = webContent.substring(matchStart, matchEnd);
-		    
-		    if (substring.contains(".png") || substring.contains(".jpg"))
-		    	continue;
+		try {
+			URL url = new URL(urlString);
+			BufferedReader contest = new BufferedReader(new InputStreamReader(url.openStream()));
 
-		    result.add(substring);
+			String line;
+			StringBuffer buffer = new StringBuffer();
+
+			while ((line = contest.readLine()) != null) {
+				buffer.append(line);
+			}
+			return buffer.toString();
+
+		} catch (IOException e) {
+			//System.err.println("Cannot download page.");
 		}
-		
+
+		return null;
+	}
+
+	private static HashSet<String> fetchAllURL(String webContent, String hostName) {
+		HashSet<String> result = new HashSet<String>();
+
+		Matcher matcher;
+		try {
+			matcher = URLPATTERN.matcher(webContent);
+		} catch (Exception e) {
+			return new HashSet<String>();
+		}
+		while (matcher.find()) {
+			int matchStart = matcher.start(1);
+			int matchEnd = matcher.end();
+
+			String substring = webContent.substring(matchStart, matchEnd);
+
+			if (substring.contains(hostName) && !substring.contains(".jpg") && !substring.contains(".js")
+					&& !substring.contains(".png") && !substring.contains(".css") && !substring.contains(".ico")) {
+				result.add(substring);
+			}
+		}
+
 		return result;
 	}
 	
-	private static ArrayList<String> filterURLs(ArrayList<String> fetchedURLs, HashSet <String> disallowed) {
-		ArrayList<String> resulting = new ArrayList<>();
-		HashSet<String> result = new HashSet <>();
+
+	private static ArrayList<String> filterURLs(HashSet<String> fetchedURLsSet, HashSet<String> disallowedURLsSet) {
 		ArrayList<String> disallowedURLs = new ArrayList<String>();
-		disallowedURLs.addAll(disallowed);
-		
+		disallowedURLs.addAll(disallowedURLsSet);
+		ArrayList<String> fetchedURLs = new ArrayList<String>();
+		fetchedURLs.addAll(fetchedURLsSet);
+		HashSet<String> result = new HashSet<>();
+		ArrayList<String> resulting = new ArrayList<>();
+
 		for (int i = 0; i < disallowedURLs.size(); i++) {
 			for (int j = 0; j < fetchedURLs.size(); j++) {
 				if (fetchedURLs.get(j).contains(disallowedURLs.get(i)))
 					fetchedURLs.remove(j);
-				else result.add(fetchedURLs.get(j));
+				else
+					result.add(fetchedURLs.get(j));
 			}
 		}
 		resulting.addAll(result);
 		return resulting;
 	}
-	
 
-	private static String downloadPage(URL url) {
-		
-		try {
-			BufferedReader contest = new BufferedReader(new InputStreamReader(url.openStream()));
-			
-			String line;
-			StringBuffer buffer = new StringBuffer();
-			
-			while ((line = contest.readLine()) != null){
-				buffer.append(line);
-			}
-			return buffer.toString();
-			
-		} catch (IOException e) {
-			System.err.println("Cannot download page.");
-		}
-		
-		return null;
-	}
-	
 }
